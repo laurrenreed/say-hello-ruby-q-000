0ed51287fbb6352354ba374e5d00789a372adb37
Lauren Reed <lauren.reed@me.com>
2018-11-12 14:25:20 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
 
-def say_hello(message = "Hello Ruby Programmer!") 
-  puts message
+def say_hello(message = "Ruby Programmer!") 
+  puts "Hello #{message}"
 end
